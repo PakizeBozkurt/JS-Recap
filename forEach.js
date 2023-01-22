982e899a40503fe967c5768e5f7b5b997e3275fc
Pakize Bozkurt <97640517+PakizeBozkurt@users.noreply.github.com>
2023-01-22 21:15:33 +0000

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,10 +1,11 @@
-//forEach!
+//forEach has condition!
 
 let numbers = [3, 56, 2, 48, 5];
 
 let newNumbers = [];
 
 numbers.forEach(function(x) {
+  // Condition!
   newNumbers.push(x * 2);
 });
 
