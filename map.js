06e6d25a3aed6b3b5e459cc0b0b17f44a9e8b6a9
Pakize Bozkurt <97640517+PakizeBozkurt@users.noreply.github.com>
2023-01-22 21:17:04 +0000

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 let numbers = [3, 56, 2, 48, 5];
 
-//Map-Created a new array by doing something with each item in ana array.
+//Map-Created a new array by doing something with each item in an array.
+//If you have an array use map function!
 
 function double(x) {
     return x * 2;
